83cd2a0e6b7c4b1853c31502aee8c2fdf7604f8d
Shatalov <yaroslav.shatalov@activeeon.com>
2017-01-05 15:20:11 +0100

    Apply coding-rules plugin

--- src/main/java/org/ow2/proactive/addons/cloud_watch/PCWUtility.java
+++ src/main/java/org/ow2/proactive/addons/cloud_watch/PCWUtility.java
@@ -1,36 +1,27 @@
 /*
- *  *
- * ProActive Parallel Suite(TM): The Java(TM) library for
- *    Parallel, Distributed, Multi-Core Computing for
- *    Enterprise Grids & Clouds
+ * ProActive Parallel Suite(TM):
+ * The Open Source library for parallel and distributed
+ * Workflows & Scheduling, Orchestration, Cloud Automation
+ * and Big Data Analysis on Enterprise Grids & Clouds.
  *
- * Copyright (C) 1997-2015 INRIA/University of
- *                 Nice-Sophia Antipolis/ActiveEon
- * Contact: proactive@ow2.org or contact@activeeon.com
+ * Copyright (c) 2007 - 2017 ActiveEon
+ * Contact: contact@activeeon.com
  *
- * This library is free software; you can redistribute it and/or
+ * This library is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation; version 3 of
+ * as published by the Free Software Foundation: version 3 of
  * the License.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Affero General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- * USA
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * If needed, contact us to obtain a release under GPL Version 2 or 3
  * or a different license than the AGPL.
- *
- *  Initial developer(s):               The ProActive Team
- *                        http://proactive.inria.fr/team_members.htm
- *  Contributor(s):
- *
- *  * $$ACTIVEEON_INITIAL_DEV$$
  */
 package org.ow2.proactive.addons.cloud_watch;
 
@@ -39,12 +30,13 @@ import java.net.HttpURLConnection;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.ow2.proactive.addons.cloud_watch.model.RestResponse;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
 import org.apache.http.entity.ContentType;
 import org.apache.http.util.EntityUtils;
+import org.ow2.proactive.addons.cloud_watch.model.RestResponse;
+
 
 public class PCWUtility {
     private static final String PCW_REST_URL = "http://localhost:8080/proactive-cloud-watch/rules/";
@@ -102,8 +94,8 @@ public class PCWUtility {
 
     private static RestResponse checkResponseIsSuccessful(RestResponse response, int successfulStatus) {
         if (response.getResponseCode() != successfulStatus) {
-            throw new RuntimeException("Failed : HTTP error code : " + response.getResponseCode()
-                    + "\n Response: " + response.getResponse());
+            throw new RuntimeException("Failed : HTTP error code : " + response.getResponseCode() + "\n Response: " +
+                                       response.getResponse());
         }
         return response;
     }
@@ -111,8 +103,7 @@ public class PCWUtility {
     protected static RestResponse executeRequest(final Request request) throws IOException {
         Response requestResponse = request.execute();
         HttpResponse responseObject = requestResponse.returnResponse();
-        return new RestResponse(
-                responseObject.getStatusLine().getStatusCode(),
-                EntityUtils.toString(responseObject.getEntity()));
+        return new RestResponse(responseObject.getStatusLine().getStatusCode(),
+                                EntityUtils.toString(responseObject.getEntity()));
     }
 }
